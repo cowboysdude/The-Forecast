4f0442d94c29f8efb95875df406cc22d08114370
John Wade <johnw8017@gmail.com>
2020-03-19 05:08:10 -0400

    Update node_helper.js

--- node_helper.js
+++ node_helper.js
@@ -6,34 +6,34 @@
  */
 var NodeHelper = require('node_helper');
 var request = require('request');
-var moment = require('moment'); 
-var fs = require('fs'); 
+var moment = require('moment');
+var fs = require('fs');
 
 module.exports = NodeHelper.create({
 
     start: function() {
         console.log("Getting module: " + this.name);
-		
-		this.forecast = {
+
+        this.forecast = {
             timestamp: null,
             data: null
         };
         this.path = "modules/The-Forecast/forecast.json";
- 
+
         if (fs.existsSync(this.path)) {
             var temp = JSON.parse(fs.readFileSync(this.path, 'utf8'));
-     
-            if (temp.timestamp === this.getDate()) { 
-				this.forecast = temp;
+
+            if (temp.timestamp === this.getDate()) {
+                this.forecast = temp;
             }
         }
-    }, 
+    },
 
-	getForecast: function(url) {
-		var self = this;
-		var forecasts;
-		request({
-             url: "https://api.weather.gov/gridpoints/BGM/35,50/forecast",
+    getForecast: function(url) {
+        var self = this;
+        var forecasts;
+        request({
+            url: "https://api.weather.gov/gridpoints/BGM/35,50/forecast",
             method: 'GET',
             headers: {
                 'token': 'SHIFYpUPJOYdfCbCOUHoueGNTdttXYGe',
@@ -41,42 +41,40 @@ module.exports = NodeHelper.create({
             },
         }, (error, response, body) => {
             var result = JSON.parse(body).properties.periods;
-            var forecast = result; 
-        console.log(forecast);    
-			
-			var farray = [];
-             forecast = forecast.slice(1, 20);     // get my results and cut it down
-        for (i = 0; i < forecast.length; i++) {     // loop the array
-            forecasts = forecast[i];  
-            var now = moment(forecasts.startTime).format('ddd');   //need to check time
-            if (forecasts.isDaytime == true) {    //set the condition that I'm looking for to create dayHigh and NightLow below
-                var high = forecasts.temperature
-            } else if (forecasts.isDaytime == false) {
-				var low = forecasts.temperature; 
-            };
-            var temps = {
-                hilow: {
-                    dayHigh: high,
-                    nightLow: low,
-                    name: forecasts.name,
-                    day: now,
-                    windDirection: forecasts.windDirection,
-                    windSpeed: forecasts.windSpeed
-                }
-            }; 
-                forecasts = Object.assign(forecasts, temps);  //Add this to each object
-	      farray.push(forecasts);  //push objects that I want into a new array.   It's only capturing the 1st one.  Everything works until I get here.....
-				
-	}
-	       //  console.log(farray);  
-				this.sendSocketNotification('FORECAST_RESULT', farray); 
-				this.forecast.timestamp = self.getDate();
-                this.forecast.data = forecasts;
-                fs.writeFileSync('forecast_array.json', JSON.stringify(this.forecast));
+            var forecast = result;
+            //console.log(forecast); 
+
+            forecast = forecast.slice(1, 20); // get my results and cut it down
+            for (i = 0; i < forecast.length; i++) { // loop the array
+                forecasts = forecast[i];
+                var now = moment(forecasts.startTime).format('ddd'); //need to check time
+                if (forecasts.isDaytime == true) { //set the condition that I'm looking for to create dayHigh and NightLow below
+                    var high = forecasts.temperature
+                } else if (forecasts.isDaytime == false) {
+                    var low = forecasts.temperature;
+                };
+                var temps = {
+                    hilow: {
+                        dayHigh: high,
+                        nightLow: low,
+                        name: forecasts.name,
+                        day: now,
+                        windDirection: forecasts.windDirection,
+                        windSpeed: forecasts.windSpeed
+                    }
+                };
+                forecasts = Object.assign(forecasts, temps); //Add this to each object  
+            }
+            this.forecast.timestamp = self.getDate();
+            this.forecast.data = forecast;
+            fs.writeFileSync(this.path, JSON.stringify(this.forecast));
+            this.sendSocketNotification('FORECAST_RESULT', this.forecast);
+            console.log(this.forecast);
+            console.log("File written");
         });
     },
 
-	fileWrite: function() {
+    fileWrite: function() {
         fs.writeFile(this.path, JSON.stringify(this.forecast), function(err) {
             if (err) {
                 return console.log(err);
@@ -88,17 +86,17 @@ module.exports = NodeHelper.create({
     getDate: function() {
         return (new Date()).toLocaleDateString();
     },
-	
-	    socketNotificationReceived: function(notification) {
-        if (notification === 'GET_FORECAST') { 
-				if (this.forecast.timestamp === this.getDate() && this.forecast.data !== null) {
+
+    socketNotificationReceived: function(notification) {
+        if (notification === 'GET_FORECAST') {
+            if (this.forecast.timestamp === this.getDate() && this.forecast.data !== null) {
                 this.sendSocketNotification('FORECAST_RESULT', this.forecast.data);
-				console.log("Using data we already have");
-            } else { 
+                console.log("Using data we already have");
+            } else {
                 this.getForecast();
-				console.log("Getting new data");
-         }   }
-	},
-	
-});	
-	
+                console.log("Getting new data");
+            }
+        }
+    },
+
+});
